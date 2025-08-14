14f151862ed6bab51baa9acd474a0cc7939f0f5e
jas09 <azharulla.mohammed1701@gmail.com>
2025-08-14 16:40:02 +0530

    Injected a script that sets a token in localStorage before the page loads.

--- test_byPassLogin.py
+++ test_byPassLogin.py
@@ -8,6 +8,14 @@ def test_session_storage(playwright: Playwright):
     getToken = api_utils.getToken(playwright)
     chromeBrowser = playwright.chromium.launch(headless=False)
     context = chromeBrowser.new_context()
+    context.add_init_script(f"""
+            // Store token in localStorage
+            window.localStorage.setItem('token', '{getToken}');
+
+            // Or in sessionStorage
+            // window.sessionStorage.setItem('token', '{getToken}');
+        """)
+    #context.add_init_script(f""""localStorage.setItem('token','{getToken}')""")
     page = context.new_page()
     #Script to inject token in session local storage
     page.add_init_script(f""""localStorage.setItem('token','{getToken}')""")
