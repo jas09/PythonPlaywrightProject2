4514df3df2d4d2b8654f421b44eb2d5c02abffec
jas09 <azharulla.mohammed1701@gmail.com>
2025-08-15 09:14:18 +0530

    - attempt2

--- pytest/test_2PyTestValidation.py
+++ pytest/test_2PyTestValidation.py
@@ -2,6 +2,6 @@ import pytest
 
 @pytest.mark.smoke
 def test_thirdCheck(preSetUpWork):
-    print("this is third test & testing GitHub-CI-Playwright")
+    print("this is third test & testing GitHub-CI-Playwright - attempt2")
 
 
